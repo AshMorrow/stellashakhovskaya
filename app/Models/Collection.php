498079170fc7ea9b4adf6a65bfbd62a11ffe5da5
Bohdan <koragg666@gmail.com>
2019-12-07 23:25:17 +0200

    larave update, move models

--- app/Models/Collection.php
+++ app/Models/Collection.php
@@ -6,8 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Collection extends Model
 {
-    protected $table = 'collection';
-
     public function dresses() {
         return $this->hasMany('\App\Dress');
     }
--- app/Models/Partition.php
+++ app/Models/Partition.php
@@ -6,8 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Partition extends Model
 {
-    protected $table = 'partition';
-
     public function collections() {
         return $this->hasMany('App\Collection');
     }
